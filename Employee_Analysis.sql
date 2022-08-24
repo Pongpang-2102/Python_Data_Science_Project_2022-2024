5fdbf6b4ac5ca1928f8a4dda8e72d8967d0c1b84
Puriwat Sangrawee <93346102+Pongpang-2102@users.noreply.github.com>
2022-08-24 17:32:23 +0700

    Update Employee_Analysis.sql

--- Employee_Analysis.sql
+++ Employee_Analysis.sql
@@ -1,2 +1,46 @@
--- 
--- Our data is under construction
+--Remark
+-- Tis case we use SQL analysis via Microsoft SQL server 2014
+-- Our Analysis use company_DB as shown in Data-Science-Project-2022 Folder
+-- but in program , we cannot rename it to descriptive name so we use the name 'GIS_PJ1'
+
+
+-- IN-Class Exercise
+
+-- to solve the problem defined
+
+--start with calling to the database (CompanyDB)
+use GIS_PJ1
+
+-- 1. Retrieve the names of all employees in 'department 5' who work more than 10 hours per week
+-- on  the 'ProductX' Project
+
+select essn , hours , Pname ,Dnum
+from EMPLOYEE , WORKS_ON , PROJECT 
+where Essn = Ssn and Pno = Pnumber
+                 and dnum = 5
+                 and Pname = 'ProductX'
+				         and Hours > 10 ;
+                
+-- 2. List top 2 employee (ssn) who work the most hours across the projects
+
+select top 2 Ssn ,
+             Fname, 
+			       Lname , 
+			       Hours
+from EMPLOYEE, WORKS_ON
+order by Hours desc
+
+--3. Find the name of Employees who are directly supervised by "Franklin Wong"
+
+select 
+
+                 
+       
+                 
+          
+         
+
+
+
+
+
